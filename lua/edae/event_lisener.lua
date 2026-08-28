0cb1a161eea997d88a8879e46c1f4e06fff9b543
CharleneHoo <CharleneHoo@hotmail.com>
2026-08-28 12:15:36 +0800

    尝试爬行

--- lua/edae/event_lisener.lua
+++ lua/edae/event_lisener.lua
@@ -8,12 +8,14 @@ local AnimationSelector = include("edae/animation_selector.lua")
 hook.Add("CreateEntityRagdoll", Constants.ADDON_NAME .. MODULE_NAME .. "CreateEntityRagdoll", function(owner, ragdoll)
     if not IsValid(owner) or not IsValid(ragdoll) then return end
 
-    local aimVector = owner:GetAimVector()
-
+    local animationName, loop = AnimationSelector:Select()
     local opts = {}
+
+    local aimVector = owner:GetAimVector()
     opts.state = "falling"
     opts.yaw = aimVector:Angle().yaw
-    local animationName = AnimationSelector:Select()
+    opts.loop = 0
+
     local ctx = AnimationPlayer:Play(ragdoll, animationName, opts)
 end)
 
