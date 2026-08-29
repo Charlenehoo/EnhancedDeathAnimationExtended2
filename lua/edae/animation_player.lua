da03912a7e34efe038df1781c6c780ad00b1611e
CharleneHoo <CharleneHoo@hotmail.com>
2026-08-29 14:47:41 +0800

    完全去掉重力代理

--- lua/edae/animation_player.lua
+++ lua/edae/animation_player.lua
@@ -12,11 +12,6 @@ local helper = include("edae/helper.lua")
 
 local AnimationPlayer = {}
 
-local DummyCollisionStrategy = {}
-function DummyCollisionStrategy:Evaluate(physObj, currentPos, desiredPos, context)
-    return "free", nil
-end
-
 local function cleanUp(ctx)
     local animationModel = ctx.animationModel
     if IsValid(animationModel) then
@@ -43,12 +38,8 @@ end
 local function playAnimationCoroutine(ctx)
     local ragdoll = ctx.ragdoll
     local animationModel = ctx.animationModel
-    local gravityProxy = ctx.gravityProxy
-    local gravityProxyPhysObj = ctx.gravityProxyPhysObj
 
-    local collisionStrategy = ctx.collisionStrategy or DummyCollisionStrategy
-
-    if not IsValid(ragdoll) or not IsValid(animationModel) or not IsValid(gravityProxy) then
+    if not IsValid(ragdoll) or not IsValid(animationModel) then
         cleanUp(ctx)
         return
     end
@@ -59,32 +50,16 @@ local function playAnimationCoroutine(ctx)
     enableMotion(ctx, true)
     coroutine.yield()
 
-    -- 第一次播放动画
     animationModel:Fire("SetAnimation", ctx.animationName)
     coroutine.yield()
 
-    while IsValid(ragdoll) and IsValid(animationModel) and IsValid(gravityProxy) and not ctx.stopSignal do
+    while IsValid(ragdoll) and IsValid(animationModel) and not ctx.stopSignal do
         if ctx.totalLoops > 0 and ctx.loopCount >= ctx.totalLoops then break end
         ctx.loopCount = ctx.loopCount + 1
         log.trace("Loop: ", ctx.loopCount, "/", ctx.totalLoops)
 
         ctx.animationEndTime = CurTime() + ctx.animationDuration
-        while CurTime() < ctx.animationEndTime do
-            local deltaTime = FrameTime()
-
-            -- 保持重力代理水平速度与布娃娃一致
-            local ragdollVelocity = ragdoll:GetVelocity()
-            local gravityProxyPhysObVelocity = gravityProxyPhysObj:GetVelocity()
-            gravityProxyPhysObj:SetVelocityInstantaneous(Vector(ragdollVelocity.x, ragdollVelocity.y,
-                gravityProxyPhysObVelocity.z))
-
-            -- 用重力代理修正动画模型实体的绝对高度（贴地）
-            local gravityProxyPhysObjPos = gravityProxyPhysObj:GetPos()
-            local currentDatumZ = gravityProxyPhysObjPos.z - ctx.gravityProxyDatumToPos.z
-            local targetZ = currentDatumZ + ctx.amDatumToPosZ
-            local amPos = animationModel:GetPos()
-            animationModel:SetPos(Vector(amPos.x, amPos.y, targetZ))
-
+        while IsValid(ragdoll) and IsValid(animationModel) and not ctx.stopSignal and CurTime() < ctx.animationEndTime do
             -- 驱动布娃娃骨骼
             for i = #ctx.boneMap, 1, -1 do
                 local boneName = ctx.boneMap[i].boneName
@@ -94,33 +69,9 @@ local function playAnimationCoroutine(ctx)
                 local amBonePos, amBoneAngle = animationModel:GetBonePosition(amBoneID)
                 if not amBonePos then continue end
 
-                local currentPos = ragdollPhysObj:GetPos()
-                local context = {
-                    ragdoll = ragdoll,
-                    animModel = animationModel,
-                    boneName = boneName,
-                    physObj = ragdollPhysObj,
-                }
-                local status, fallbackPos = collisionStrategy:Evaluate(
-                    ragdollPhysObj,
-                    currentPos,
-                    amBonePos,
-                    context
-                )
-
                 local shadowParams = ctx.shadowParamsTemplate
-
-                if status == "free" then
-                    shadowParams.pos = amBonePos
-                    shadowParams.angle = amBoneAngle
-                elseif status == "fallback" then
-                    shadowParams.pos = fallbackPos
-                    shadowParams.angle = amBoneAngle
-                else
-                    table.remove(ctx.boneMap, i)
-                    continue
-                end
-
+                shadowParams.pos = amBonePos
+                shadowParams.angle = amBoneAngle
                 ragdollPhysObj:Wake()
                 ragdollPhysObj:ComputeShadowControl(shadowParams)
             end
@@ -132,7 +83,7 @@ local function playAnimationCoroutine(ctx)
             start = ragdollPos + Vector(0, 0, 50),
             endpos = ragdollPos - Vector(0, 0, 100),
             mask = MASK_SOLID,
-            filter = { ragdoll, animationModel, gravityProxy }
+            filter = { ragdoll, animationModel }
         })
 
         local groundPos = trace.Hit and trace.HitPos or ragdollPos
@@ -153,32 +104,6 @@ local function fillShadowParamsTemplate(ctx)
         end
     end
     ctx.shadowParamsTemplate = shadowParams
-    return true
-end
-
-local function creatGravityProxy(ctx)
-    local gravityProxy = ents.Create("prop_sphere")
-    ctx.gravityProxy = gravityProxy
-
-
-    local radius = ctx.gravityProxyRadius
-    ctx.gravityProxyDatumToPos = Vector(0, 0, radius)
-
-    gravityProxy:SetKeyValue("radius", tostring(radius))
-    gravityProxy:SetModel(ctx.gravityProxyModelName)
-    gravityProxy:Spawn()
-
-    gravityProxy:SetPos(ctx.datum + ctx.gravityProxyDatumToPos)
-    gravityProxy:SetCustomCollisionCheck(true)
-    gravityProxy:SetFriction(0)
-    gravityProxy:SetGravity(10)
-
-    local gravityProxyPhysObj = gravityProxy:GetPhysicsObject()
-    ctx.gravityProxyPhysObj = gravityProxyPhysObj
-
-    gravityProxyPhysObj:EnableDrag(false)
-    gravityProxyPhysObj:EnableGravity(true)
-
     return true
 end
 
@@ -300,7 +225,6 @@ function AnimationPlayer:Play(ragdoll, animationName, opts)
             Constants.ANIMATION_PLAYER.ANIMATION_MODEL.REF_BONE_NAME,
         gravityProxyModelName     = opts.gravityProxyModelName or Constants.ANIMATION_PLAYER.GRAVITY_PROXY.MODEL_NAME,
         gravityProxyRadius        = opts.radius or Constants.ANIMATION_PLAYER.GRAVITY_PROXY.RADIUS,
-        collisionStrategy         = opts.collisionStrategy or DummyCollisionStrategy,
 
         -- ===============================
         -- 以下由 createAnimationModel 填充
@@ -326,13 +250,6 @@ function AnimationPlayer:Play(ragdoll, animationName, opts)
         -- ===============================
 
         -- ===============================
-        -- 以下由 creatGravityProxy 填充
-        gravityProxy              = nil,
-        gravityProxyPhysObj       = nil,
-        gravityProxyDatumToPos    = nil,
-        -- ===============================
-
-        -- ===============================
         -- 以下由 fillShadowParamsTemplate 填充
         shadowParamsTemplate      = opts.shadowParamsTemplate,
         -- ===============================
@@ -348,7 +265,6 @@ function AnimationPlayer:Play(ragdoll, animationName, opts)
         not checkAnimationName(ctx) or
         not alignAnimationModel(ctx) or
         not makeBoneMap(ctx) or
-        not creatGravityProxy(ctx) or
         not fillShadowParamsTemplate(ctx)
     then
         cleanUp(ctx)
@@ -362,18 +278,6 @@ function AnimationPlayer:Play(ragdoll, animationName, opts)
 
     return ctx
 end
-
-hook.Add("ShouldCollide", Constants.ADDON_NAME .. MODULE_NAME .. "ShouldCollide", function(ent1, ent2)
-    if ent1[Constants.RAGDOLL_CONTEXT_KEY] then
-        return ent2 ~= ent1[Constants.RAGDOLL_CONTEXT_KEY].gravityProxy
-    end
-
-    if ent2[Constants.RAGDOLL_CONTEXT_KEY] then
-        return ent1 ~= ent2[Constants.RAGDOLL_CONTEXT_KEY].gravityProxy
-    end
-
-    return true
-end)
 
 _EnhancedDeathAnimationExtendedSingletons[MODULE_NAME] = AnimationPlayer
 return AnimationPlayer
