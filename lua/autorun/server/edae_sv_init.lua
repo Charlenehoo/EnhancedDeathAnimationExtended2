331556f5f9cb2e4ed8d5e345224f85228356e45c
CharleneHoo <CharleneHoo@hotmail.com>
2026-08-31 15:05:10 +0800

    AI 1

--- lua/autorun/server/edae_sv_init.lua
+++ lua/autorun/server/edae_sv_init.lua
@@ -2,5 +2,5 @@ _EnhancedDeathAnimationExtendedSingletons = _EnhancedDeathAnimationExtendedSingl
 
 -- include("edae/event_lisener.lua")
 
-include("edae/rm/core.lua")
+include("edae/rm/ragdoll_manager.lua")
 include("edae/pcpr/player_create_prop_ragdoll.lua")
--- lua/edae/rm/ragdoll_manager.lua
+++ lua/edae/rm/ragdoll_manager.lua
@@ -1,8 +1,13 @@
+-- lua\edae\rm\ragdoll_manager.lua
+
 local MODULE_NAME = "RagdollManager"
 
 local Constants = include("edae/config/constants.lua")
 local log = include("edae/log/init.lua")
 local EntityDataStore = include("edae/eds/entity_data_store.lua")
+local DamageContextManager = include("edae/dcm/damage_context_manager.lua")
+local AnimationSelector = include("edae/as/animation_selector.lua")
+local AnimationPlayer = include("edae/ap/animation_player.lua")
 local LifeCycleHandler = include("edae/lch/life_cycle_handler.lua")
 
 local store = EntityDataStore:ForOwner(MODULE_NAME)
@@ -20,18 +25,39 @@ function Manager:SetHealth(ragdoll, health)
     return store:Set(ragdoll, HEALTH_KEY, health)
 end
 
+-- ======================================
+
 function Manager:OnCreate(owner, ragdoll)
-    store:SetHealth(ragdoll, MAX_HEALTH)
-    LifeCycleHandler:Init(ragdoll)
+    self:SetHealth(ragdoll, MAX_HEALTH)
+    local damageContext = DamageContextManager:Get(owner)
+    local state = LifeCycleHandler:Init(ragdoll, damageContext)
+    local animationName, totalLoops, secondsBeforePlay = AnimationSelector:Select(state, damageContext)
+    local opts = {}
+    opts.totalLoops = totalLoops
+    opts.secondsBeforePlay = secondsBeforePlay
+    AnimationPlayer:Play(ragdoll, animationName, opts)
 end
 
 function Manager:OnTakeDamage(ragdoll, dmginfo)
     local dmg = dmginfo:GetDamage()
-    local healthBefore = store:Get(ragdoll, HEALTH_KEY)
+    local healthBefore = self:GetHealth(ragdoll)
     local healthAfter = healthBefore - dmg
-    store:Set(ragdoll, HEALTH_KEY, healthAfter)
+    self:SetHealth(ragdoll, healthAfter)
     LifeCycleHandler:DetermineState(ragdoll)
 end
+
+function Manager:OnStateChange(ragdoll, state)
+    if AnimationPlayer:IsPlaying(ragdoll) then
+        AnimationPlayer:Stop(ragdoll)
+    end
+    local animationName, totalLoops, secondsBeforePlay = AnimationSelector:Select(state, nil)
+    local opts = {}
+    opts.totalLoops = totalLoops
+    opts.secondsBeforePlay = secondsBeforePlay
+    AnimationPlayer:Play(ragdoll, animationName, opts)
+end
+
+-- ======================================
 
 hook.Add("CreateEntityRagdoll", Constants.ADDON_NAME .. MODULE_NAME .. "CreateEntityRagdoll", function(owner, ragdoll)
     if not IsValid(owner) then return end
