add656b79f37908894fbd1d2006ae2d655dee31d
CharleneHoo <CharleneHoo@hotmail.com>
2026-08-31 10:32:06 +0800

    修复 Bug

--- lua/pcpr/core.lua
+++ lua/pcpr/core.lua
@@ -15,24 +15,81 @@ if not originalCreateRagdoll then
     return
 end
 
+local originalGetRagdollEntity = meta.GetRagdollEntity
+if not originalGetRagdollEntity then
+    log.fatal("Cannot find function: Player.GetRagdollEntity")
+    return
+end
+
 local plyToRagdollMap = {}
 
 hook.Add("PlayerDisconnected", Constants.ADDON_NAME .. MODULE_NAME .. "PlayerDisconnected", function(ply)
     plyToRagdollMap[ply] = nil
 end)
 
+meta.GetRagdollEntity = function(self)
+    return plyToRagdollMap[self] or originalGetRagdollEntity(self)
+end
+
+local cleanUp = function(ply)
+    local ragdoll = ply:GetRagdollEntity()
+    if IsValid(ragdoll) then
+        ragdoll:Remove()
+    end
+    plyToRagdollMap[ply] = nil
+    return originalCreateRagdoll(ply)
+end
+
 meta.CreateRagdoll = function(self)
     if not IsValid(self) or not self:IsPlayer() then
-        return originalCreateRagdoll(self)
-    end
-    local ply = self
-
-    local plyModelName = ply:GetModel()
-    if not plyModelName then
-        log.warn("")
-        return originalCreateRagdoll(self)
+        return cleanUp(self)
     end
 
+    local plyModel = self:GetModel()
+    if not plyModel then
+        log.warn("Cannot find model for player: ", self)
+        return cleanUp(self)
+    end
 
-    -- local ragdoll = ents.Create("prop_ragdoll")
+    local ragdoll = ents.Create("prop_ragdoll")
+    plyToRagdollMap[self] = ragdoll
+
+    ragdoll:SetModel(plyModel)
+    local ragdollModel = ragdoll:GetModel()
+    if not ragdollModel or ragdollModel ~= plyModel then
+        log.warn("Cannot set model for player: ", self, "; Ragdoll: ", ragdoll)
+        return cleanUp(self)
+    end
+
+    ragdoll:Spawn()
+    if not IsValid(ragdoll) then
+        return cleanUp(self)
+    end
+
+    local physicsObjectCount = ragdoll:GetPhysicsObjectCount()
+    if not physicsObjectCount or physicsObjectCount < 1 then
+        return cleanUp(self)
+    end
+
+    for physObjNum = 0, physicsObjectCount - 1 do
+        local boneID = ragdoll:TranslatePhysBoneToBone(physObjNum)
+        if not boneID then continue end
+
+        local pos, ang = self:GetBonePosition(boneID)
+        if not pos then continue end
+
+        local physObj = ragdoll:GetPhysicsObjectNum(physObjNum)
+        if not physObj then continue end
+
+        physObj:SetPos(pos, true)
+        physObj:SetAngles(ang)
+        physObj:EnableMotion(true)
+        physObj:Wake()
+    end
+
+    self:SpectateEntity(ragdoll)
+
+    hook.Run("CreateEntityRagdoll", self, ragdoll)
+
+    return ragdoll
 end
